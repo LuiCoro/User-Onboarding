b812f041ab8858a11616238fb53adaec36cf78d2
LuiCoro <luiacorona029@icloud.com>
2021-08-12 14:27:38 -0700

    'Added' props being sent to form

--- users/src/App.js
+++ users/src/App.js
@@ -2,10 +2,27 @@ import React, { useState } from 'react';
 
 import Form from './Components/Form';
 
+const defaultValue = [
+	{
+		name: '',
+		email: '',
+		password: '',
+		serviceTerm: false,
+	},
+];
+
+const defaultUser = [];
+
 const App = () => {
+	const [formValues, setFormValues] = useState(defaultValue);
+	const [users, setUsers] = useState(defaultUser);
+
+	console.log('Default User', defaultUser);
+	console.log('Form Data', formValues);
+
 	return (
 		<div>
-			<Form />
+			<Form values={formValues} />
 		</div>
 	);
 };
--- users/src/Components/Form.js
+++ users/src/Components/Form.js
@@ -1,30 +1,60 @@
 import React from 'react';
 
-const Form = () => {
+const Form = props => {
+	const { values } = props;
+
+	const onChange = evt => {};
+
 	return (
 		<div>
 			<form>
 				<label>
-					Name : <input type='text' placeholder='Enter Name' name='name' />
+					Name :{' '}
+					<input
+						type='text'
+						placeholder='Enter Name'
+						name='name'
+						onChange={onChange}
+						value={values.name}
+					/>
 				</label>
 
 				<br />
 
 				<label>
-					Email : <input type='email' placeholder='Enter Email' email='email' />
+					Email :{' '}
+					<input
+						type='email'
+						placeholder='Enter Email'
+						name='email'
+						onChange={onChange}
+						value={values.email}
+					/>
 				</label>
 
 				<br />
 
 				<label>
 					Password :
-					<input type='password' placeholder='Enter Password' name='password' />
+					<input
+						type='password'
+						placeholder='Enter Password'
+						name='password'
+						onChange={onChange}
+						value={values.password}
+					/>
 				</label>
 
 				<br />
 
 				<label>
-					Terms of Service <input type='checkbox' name='terms' />
+					Terms of Service{' '}
+					<input
+						type='checkbox'
+						name='terms'
+						onChange={onChange}
+						checked={values.serviceTerm}
+					/>
 				</label>
 
 				<br />
