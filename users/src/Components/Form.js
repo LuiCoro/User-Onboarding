74f026dd605dc76ddab36d2faa839b3545d89060
LuiCoro <luiacorona029@icloud.com>
2021-08-12 08:40:40 -0700

    'Setting Up' Form w/ all mvp requirements

--- users/src/Components/Form.js
+++ users/src/Components/Form.js
@@ -3,7 +3,34 @@ import React from 'react';
 const Form = () => {
 	return (
 		<div>
-			<h1>Hello world</h1>
+			<form>
+				<label>
+					Name : <input type='text' placeholder='Enter Name' name='name' />
+				</label>
+
+				<br />
+
+				<label>
+					Email : <input type='email' placeholder='Enter Email' email='email' />
+				</label>
+
+				<br />
+
+				<label>
+					Password :
+					<input type='password' placeholder='Enter Password' name='password' />
+				</label>
+
+				<br />
+
+				<label>
+					Terms of Service <input type='checkbox' name='terms' />
+				</label>
+
+				<br />
+
+				<button>Add User</button>
+			</form>
 		</div>
 	);
 };
